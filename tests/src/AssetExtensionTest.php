d8a642e0413c9a908a97ffbc953494db29ee972c
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-09-20 13:41:48 +0200

    allow absolute paths

--- tests/src/AssetExtensionTest.php
+++ tests/src/AssetExtensionTest.php
@@ -24,7 +24,14 @@ class AssetExtensionTest extends TestCase
         $stub = $this->createMock(Packages::class);
         $stub->method('getUrl')
             ->willReturnCallback(function ($path) {
-                return 'https://Ixocreate' . $path;
+                /**
+                 * Do not prefix absolute urls
+                 * This aligns with symfony asset package behaviour
+                 */
+                if (\mb_strpos($path, 'http') === 0) {
+                    return $path;
+                }
+                return 'https://ixocreate.test/' . \ltrim($path, '/');
             });
         $asset = new Asset($stub);
         $assetExtension = new AssetExtension($asset);
@@ -32,6 +39,9 @@ class AssetExtensionTest extends TestCase
         $this->assertInstanceOf(AssetExtension::class, $assetExtension);
 
         $this->assertSame('asset', $assetExtension->getName());
-        $this->assertSame('https://Ixocreate/Asset/', $assetExtension('Asset/'));
+        $this->assertSame('https://ixocreate.test/assets/', $assetExtension('assets/'));
+        $this->assertSame('https://ixocreate.test/assets/', $assetExtension('/assets/'));
+
+        $this->assertSame('https://ixocreate.test/assets/', $assetExtension('https://ixocreate.test/assets/'));
     }
 }
